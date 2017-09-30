edbcce208fba1ab7aab7a5247cd662137a7b5d0a
Christopher R. Wicks <wickstopher@gmail.com>
2017-09-30 19:12:46 -0400

    Parsing frames from socket.

--- StompBroker.hs
+++ StompBroker.hs
@@ -3,6 +3,7 @@ import Data.ByteString as BS
 import Data.ByteString.UTF8 as UTF
 import Data.ByteString.Char8 as Char8
 import Data.List.Split
+import Data.Word
 import Network.Socket
 import Stomp.Frames
 import System.IO as IO
@@ -32,25 +33,25 @@ handleConnection handle = do
 processConnectFrame :: Handle -> IO ()
 processConnectFrame handle = do
     frame <- parseFrame handle
-    putStrLn (show frame)
+    IO.putStrLn (show frame)
 
 parseFrame :: Handle -> IO Frame
 parseFrame handle = do
     command <- getCommand handle
     headers <- getHeaders handle EndOfHeaders
-    body <- getBody handle (getContentLength headers)
-    return (Frame command headers body)
+    body    <- getBody handle (getContentLength headers)
+    return $ Frame command headers body
 
 getCommand :: Handle -> IO Command
 getCommand handle = do
     commandLine <- BS.hGetLine handle
-    return stringToCommand (commandLine)
+    return $ stringToCommand (toString commandLine)
 
 getHeaders :: Handle -> Headers -> IO Headers
 getHeaders handle headers = do
     line <- stringFromHandle handle
-    if line == ""
-        then return headers
+    if line == "" then
+        return headers
     else
         getHeaders handle (addHeaderEnd (headerFromLine line) headers)
 
@@ -60,24 +61,29 @@ getBody handle (Just n) = do
     nullByte <- hGet handle 1
     return (Body bytes)
 getBody handle Nothing  = do 
+    bytes <- hGet handle 1
+    if bytes == (fromString "\NUL") then
+        return EmptyBody
+    else do
+        body <- getBodyNoContentLengthHeader handle [BS.head bytes]
+        return $ Body body
+
+getBodyNoContentLengthHeader :: Handle -> [Word8] -> IO ByteString
+getBodyNoContentLengthHeader handle bytes = do
     byte <- hGet handle 1
-    if (toString byte) == "\NUL" then return EmptyBody
+    if byte == (fromString "\NUL") then
+        return (BS.pack $ Prelude.reverse bytes)
+    else
+        getBodyNoContentLengthHeader handle ((BS.head byte) : bytes)
 
-doIt "CONNECT" = do
-    IO.putStrLn "Yeah!"
-doIt "\NUL" = do
-    IO.putStrLn "nully!"
-doIt _ = 
-    IO.putStrLn "nahhh"
-
-tokenize :: String -> [String]
-tokenize = Prelude.filter (not . Prelude.null) . splitOn " "
+tokenize :: String -> String -> [String]
+tokenize delimiter = Prelude.filter (not . Prelude.null) . splitOn delimiter
 
 headerFromLine :: String -> Header
-headerFromLine line = let tokens = tokenize line in
+headerFromLine line = let tokens = tokenize ":" line in
     Header (Prelude.head tokens) (Prelude.last tokens)
 
 stringFromHandle :: Handle -> IO String
 stringFromHandle handle = do
     line <- BS.hGetLine handle
-    return (toString line)
+    return $ toString line
