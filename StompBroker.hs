73fd923053e12e289671063e063d2a479e2a6268
Christopher R. Wicks <wickstopher@gmail.com>
2017-09-30 18:12:13 -0400

    Check in of WIP frame-parsing code.

--- StompBroker.hs
+++ StompBroker.hs
@@ -1,6 +1,11 @@
 import Control.Concurrent
+import Data.ByteString as BS
+import Data.ByteString.UTF8 as UTF
+import Data.ByteString.Char8 as Char8
+import Data.List.Split
 import Network.Socket
-import System.IO
+import Stomp.Frames
+import System.IO as IO
 
 main :: IO ()
 main = do
@@ -8,7 +13,7 @@ main = do
     setSocketOption sock ReuseAddr 1
     bind sock (SockAddrInet 2323 iNADDR_ANY)
     listen sock 5
-    putStrLn "STOMP broker initiated on port 2323"
+    IO.putStrLn "STOMP broker initiated on port 2323"
     socketLoop sock
 
 socketLoop :: Socket -> IO ()
@@ -21,6 +26,58 @@ socketLoop sock = do
 handleConnection :: Handle -> IO ()
 handleConnection handle = do
     hSetBuffering handle NoBuffering
-    input <- hGetLine handle
-    putStrLn input
-    handleConnection handle
+    processConnectFrame handle
+
+
+processConnectFrame :: Handle -> IO ()
+processConnectFrame handle = do
+    frame <- parseFrame handle
+    putStrLn (show frame)
+
+parseFrame :: Handle -> IO Frame
+parseFrame handle = do
+    command <- getCommand handle
+    headers <- getHeaders handle EndOfHeaders
+    body <- getBody handle (getContentLength headers)
+    return (Frame command headers body)
+
+getCommand :: Handle -> IO Command
+getCommand handle = do
+    commandLine <- BS.hGetLine handle
+    return stringToCommand (commandLine)
+
+getHeaders :: Handle -> Headers -> IO Headers
+getHeaders handle headers = do
+    line <- stringFromHandle handle
+    if line == ""
+        then return headers
+    else
+        getHeaders handle (addHeaderEnd (headerFromLine line) headers)
+
+getBody :: Handle -> Maybe Int -> IO Body
+getBody handle (Just n) = do
+    bytes <- hGet handle n
+    nullByte <- hGet handle 1
+    return (Body bytes)
+getBody handle Nothing  = do 
+    byte <- hGet handle 1
+    if (toString byte) == "\NUL" then return EmptyBody
+
+doIt "CONNECT" = do
+    IO.putStrLn "Yeah!"
+doIt "\NUL" = do
+    IO.putStrLn "nully!"
+doIt _ = 
+    IO.putStrLn "nahhh"
+
+tokenize :: String -> [String]
+tokenize = Prelude.filter (not . Prelude.null) . splitOn " "
+
+headerFromLine :: String -> Header
+headerFromLine line = let tokens = tokenize line in
+    Header (Prelude.head tokens) (Prelude.last tokens)
+
+stringFromHandle :: Handle -> IO String
+stringFromHandle handle = do
+    line <- BS.hGetLine handle
+    return (toString line)
