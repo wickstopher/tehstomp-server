17a08945b079a1f03da44c2d054bb71b8c1c4225
Christopher R. Wicks <wickstopher@gmail.com>
2017-10-01 15:28:56 -0400

    Handle incoming CONNECT frames.

--- StompBroker.hs
+++ StompBroker.hs
@@ -1,12 +1,10 @@
 import Control.Concurrent
 import Data.ByteString as BS
-import Data.ByteString.UTF8 as UTF
-import Data.ByteString.Char8 as Char8
-import Data.List.Split
-import Data.Word
+import Data.List as List
+import Stomp.Frames.IO
 import Network.Socket
-import Stomp.Frames
 import System.IO as IO
+import Stomp.Frames
 
 main :: IO ()
 main = do
@@ -21,13 +19,56 @@ socketLoop :: Socket -> IO ()
 socketLoop sock = do
     (uSock, _) <- accept sock
     handle <- socketToHandle uSock ReadWriteMode
-    forkIO (handleConnection handle)
+    forkIO (negotiateConnection handle)
     socketLoop sock
 
-handleConnection :: Handle -> IO ()
-handleConnection handle = do
+negotiateConnection :: Handle -> IO ()
+negotiateConnection handle = do
     hSetBuffering handle NoBuffering
-    processFrames handle
+    frame <- parseFrame handle
+    case (getCommand frame) of
+        STOMP   -> handleNewConnection handle frame
+        CONNECT -> handleNewConnection handle frame
+        _       -> rejectConnection handle "Please initiate communications with a connection request"
+    
+handleNewConnection :: Handle -> Frame -> IO ()
+handleNewConnection handle frame = let version = determineVersion frame in
+    case version of
+        Just v  -> do 
+            sendConnectedResponse handle v
+            connectionLoop handle
+        Nothing -> rejectConnection handle ("Supported STOMP versions are: " ++  supportedVersionsAsString)
+
+connectionLoop :: Handle -> IO ()
+connectionLoop handle = do
+    frame <- parseFrame handle
+    handleFrame handle frame
+    connectionLoop handle
+
+sendConnectedResponse :: Handle -> String -> IO ()
+sendConnectedResponse handle version = let response = connected version in
+    do hPut handle $ frameToBytes response
+
+rejectConnection :: Handle -> String -> IO ()
+rejectConnection handle message = let response = errorFrame message in
+    do  hPut handle $ frameToBytes response
+        hClose handle
+
+determineVersion :: Frame -> Maybe String
+determineVersion frame = 
+    let clientVersions = getSupportedVersions frame in
+        case clientVersions of 
+            Just versions -> getHighestSupportedVersion versions
+            Nothing       -> Nothing
+
+getHighestSupportedVersion :: [String] -> Maybe String
+getHighestSupportedVersion clientVersions = 
+    let mutualVersions = intersect clientVersions supportedVersions in
+        maybeMax mutualVersions
+
+maybeMax :: Ord a => [a] -> Maybe a
+maybeMax [] = Nothing
+maybeMax xs = Just (List.maximum xs)
 
 processFrames :: Handle -> IO ()
 processFrames handle = do
@@ -35,55 +76,15 @@ processFrames handle = do
     IO.putStrLn $ show frame
     processFrames handle
 
-parseFrame :: Handle -> IO Frame
-parseFrame handle = do
-    command <- getCommand handle
-    headers <- getHeaders handle EndOfHeaders
-    body    <- getBody handle (getContentLength headers)
-    return $ Frame command headers body
+supportedVersions :: [String]
+supportedVersions = ["1.2"]
 
-getCommand :: Handle -> IO Command
-getCommand handle = do
-    commandLine <- BS.hGetLine handle
-    return $ stringToCommand (toString commandLine)
+intercalcate :: String -> [String] -> String
+intercalcate separator strings  = List.concat (List.intersperse separator strings)
 
-getHeaders :: Handle -> Headers -> IO Headers
-getHeaders handle headers = do
-    line <- stringFromHandle handle
-    if line == "" then
-        return headers
-    else
-        getHeaders handle (addHeaderEnd (headerFromLine line) headers)
+supportedVersionsAsString :: String
+supportedVersionsAsString = intercalcate ", " supportedVersions
 
-getBody :: Handle -> Maybe Int -> IO Body
-getBody handle (Just n) = do
-    bytes <- hGet handle n
-    nullByte <- hGet handle 1
-    return (Body bytes)
-getBody handle Nothing  = do 
-    bytes <- hGet handle 1
-    if bytes == (fromString "\NUL") then
-        return EmptyBody
-    else do
-        body <- getBodyNoContentLengthHeader handle [BS.head bytes]
-        return $ Body body
-
-getBodyNoContentLengthHeader :: Handle -> [Word8] -> IO ByteString
-getBodyNoContentLengthHeader handle bytes = do
-    byte <- hGet handle 1
-    if byte == (fromString "\NUL") then
-        return (BS.pack $ Prelude.reverse bytes)
-    else
-        getBodyNoContentLengthHeader handle ((BS.head byte) : bytes)
-
-tokenize :: String -> String -> [String]
-tokenize delimiter = Prelude.filter (not . Prelude.null) . splitOn delimiter
-
-headerFromLine :: String -> Header
-headerFromLine line = let tokens = tokenize ":" line in
-    Header (Prelude.head tokens) (Prelude.last tokens)
-
-stringFromHandle :: Handle -> IO String
-stringFromHandle handle = do
-    line <- BS.hGetLine handle
-    return $ toString line
+handleFrame :: Handle -> Frame -> IO ()
+handleFrame handle frame = do
+    IO.putStrLn (show frame)
