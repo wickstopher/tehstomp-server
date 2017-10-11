6055c0512201804333857d21b06776445b56fc65
Christopher R. Wicks <wickstopher@gmail.com>
2017-10-11 19:55:43 -0400

    Add functions to interact with notifier.

--- Subscription.hs
+++ Subscription.hs
@@ -1,4 +1,9 @@
-module Subscription where
+module Subscription (
+    initNotifier,
+    addSubscription,
+    removeSubscription,
+    reportMessage
+) where
 
 import Control.Concurrent
 import Control.Concurrent.TxEvent
@@ -8,29 +13,31 @@ import Data.Unique
 import Stomp.Frames
 import Stomp.Frames.IO
 
-type Dest= String
-type ClientId = Integer
-type SubscriptionId = String
+type Dest                  = String
+type SubscriptionId        = String
+type ClientId              = Integer
 
-data Client = Client ClientId FrameHandler SubscriptionId
+data Subscription          = Subscription ClientId FrameHandler SubscriptionId Dest
 
-data Topic = Topic (HashMap Integer Client)
+data Topic                 = Topic (HashMap Integer Subscription)
 
-data Destinations = Destinations (HashMap Dest Topic)
+data Destinations          = Destinations (HashMap Dest Topic)
 
-data Update = Add Client Dest |
-              Remove Client Dest |
-              GotMessage Dest Frame
+data Update                = Add Subscription |
+                             Remove Subscription |
+                            GotMessage Dest Frame
 
-data SubscriptionNotifier = SubscriptionNotifier (SChan Update)
+data SubscriptionNotifier  = SubscriptionNotifier (SChan Update)
 
 data SubscriptionException = ClientNotSubscribed Dest |
-                             DestinationDoesNotExist Dest
+                             DestinationDoesNotExist Dest |
+                             NoDestinationHeader
 
 instance Exception SubscriptionException
 instance Show SubscriptionException where
     show (ClientNotSubscribed dest)     = "Client is not subscribed to " ++ dest
     show (DestinationDoesNotExist dest) = "Destination " ++ dest ++ " does not exist"
+    show NoDestinationHeader            = "Received a frame without a destination header"
 
 initNotifier :: IO SubscriptionNotifier
 initNotifier = do
@@ -46,38 +53,53 @@ notificationLoop updateChan destinations = do
     notificationLoop updateChan destinations'
 
 handleUpdate :: Update -> Destinations -> IO Destinations
-handleUpdate (Add client dest) d@(Destinations topicMap) = 
-    return $ Destinations $ insert dest (addClient d client dest) topicMap
-handleUpdate (Remove client dest) d@(Destinations topicMap) = 
-    return $ Destinations $ insert dest (removeClient d client dest) topicMap
+handleUpdate (Add sub@(Subscription _ _ _ dest)) d@(Destinations topicMap) = 
+    return $ Destinations $ insert dest (addSub d sub) topicMap
+handleUpdate (Remove sub@(Subscription _ _ _ dest)) d@(Destinations topicMap) = 
+    return $ Destinations $ insert dest (removeSub d sub) topicMap
 handleUpdate (GotMessage dest frame) d@(Destinations topicMap) = do
     case HM.lookup dest topicMap of
-        Just (Topic clientMap) -> sendMessageToClients frame clientMap
-        Nothing                -> throw $ DestinationDoesNotExist dest
+        Just (Topic subMap) -> sendMessageToSubs frame subMap
+        Nothing             -> throw $ DestinationDoesNotExist dest
     return d
 
-addClient :: Destinations -> Client -> Dest -> Topic
-addClient d@(Destinations topicMap) c@(Client cid _ _) dest =
+addSub :: Destinations -> Subscription -> Topic
+addSub d@(Destinations topicMap) sub@(Subscription subId _ _ dest) =
     case HM.lookup dest topicMap of
-            Just (Topic clientMap) -> Topic (HM.insert cid c clientMap)
-            Nothing                -> Topic (singleton cid c)
+            Just (Topic subMap) -> Topic (HM.insert subId sub subMap)
+            Nothing             -> Topic (singleton subId sub)
 
-removeClient :: Destinations -> Client -> Dest -> Topic
-removeClient d@(Destinations topicMap) c@(Client cid _ _) dest =
+removeSub :: Destinations -> Subscription -> Topic
+removeSub d@(Destinations topicMap) sub@(Subscription subId _ _ dest) =
     case HM.lookup dest topicMap of
-        Just (Topic clientMap) -> Topic (HM.delete cid clientMap)
-        Nothing                -> throw $ ClientNotSubscribed dest
+        Just (Topic subMap) -> Topic (HM.delete subId subMap)
+        Nothing             -> throw $ ClientNotSubscribed dest
 
-sendMessageToClients :: Frame -> (HashMap Integer Client) -> IO ()
-sendMessageToClients frame clientMap = mapM_ (sendMessage frame) clientMap
+sendMessageToSubs :: Frame -> (HashMap Integer Subscription) -> IO ()
+sendMessageToSubs frame subMap = mapM_ (sendMessage frame) subMap
 
-sendMessage :: Frame -> Client -> IO ()
-sendMessage frame c@(Client _ handler subscriptionId) = do
+sendMessage :: Frame -> Subscription -> IO ()
+sendMessage frame sub@(Subscription _ handler subId _) = do
     unique <- newUnique
     frame' <- return $ addFrameHeaderFront (messageIdHeader $ show $ hashUnique unique) frame
-    forkIO $ put handler (transformMessage frame' c)
+    forkIO $ put handler (transformMessage frame' sub)
     return ()
 
-transformMessage :: Frame -> Client -> Frame
-transformMessage (Frame _ headers body) (Client _ _ subscriptionId) = 
-    Frame MESSAGE (addHeaderFront (subscriptionHeader subscriptionId) headers) body
+transformMessage :: Frame -> Subscription -> Frame
+transformMessage (Frame _ headers body) (Subscription _ _ subId _) = 
+    Frame MESSAGE (addHeaderFront (subscriptionHeader subId) headers) body
+
+addSubscription :: SubscriptionNotifier -> ClientId -> FrameHandler -> SubscriptionId -> Dest -> IO Subscription
+addSubscription (SubscriptionNotifier chan) clientId handler subId dest = 
+    let subscription = (Subscription clientId handler subId dest) in do
+        sync $ sendEvt chan (Add subscription)
+        return subscription
+
+removeSubscription :: SubscriptionNotifier -> Subscription -> IO ()
+removeSubscription (SubscriptionNotifier chan) client = sync $ sendEvt chan (Remove client)
+
+reportMessage :: SubscriptionNotifier -> Frame -> IO ()
+reportMessage (SubscriptionNotifier chan) frame = 
+    case (getDestination frame) of
+        Just dest -> sync $ sendEvt chan (GotMessage dest frame)
+        Nothing   -> throw NoDestinationHeader
